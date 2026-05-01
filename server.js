e2b6ace7fd034477f32b42ba498b29532c6a0fe2
boio-bit <boiolapica@gmail.com>
2026-05-01 13:58:58 -0300

    fix login system clean

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.json());
+
+// 🔥 serve tudo do client automaticamente
 app.use(express.static(path.join(__dirname, "client")));
 
+// 🔥 sempre abre login
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
-app.get("/{*splat}", (req, res) => {
+// 🔥 fallback (IMPORTANTE pro render)
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("rodando");
+  console.log("Block Jogos rodando");
 });
